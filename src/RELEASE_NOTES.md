fd2db34c4db5930968add80ca0dd0e4fc0b2bc7f
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2017-04-05 14:23:44 +0100

    added a few more custommizations

---
+++ src/RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+### 0.0.1-alpha - April 1 2017
+
+* Project start
--- src/build.fsx
+++ src/build.fsx
@@ -1,17 +1,91 @@
-#r @"packages/__commandlinetools/FAKE/tools/FakeLib.dll"
-open Fake
+(*
+ * Generic build template using Fake
+ *
+ * It follows the general template from https://github.com/fsprojects/ProjectScaffold/blob/master/build.template
+ *)
 
-let buildDir = "../build/"
-let testDir = "../test/"
-let deployDir= "../deploy/"
+#r @"packages/__commandlinetools/FAKE/tools/FakeLib.dll"
+
+// #r @"packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+#I @"packages/Newtonsoft.Json/lib/net45"
+#r "Newtonsoft.Json.dll"
+
+open System
+open System.IO
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open Fake.UserInputHelper
+open Newtonsoft.Json
+
+(*
+ * Read configuration variables
+ *)
+
+let projectTopDir = __SOURCE_DIRECTORY__
+let projectDescriptionFile = "Project.Description.json"
+
+type ProjectDescription =
+    {
+        Summary : string
+        Description : string
+        Authors : string list
+        Tags : string list
+        RepoHome : string
+    }
+
+let project =
+    let fullname = Path.Combine (projectTopDir, projectDescriptionFile)
+    if File.Exists fullname then
+        let project = JsonConvert.DeserializeObject<ProjectDescription>(File.ReadAllText(fullname))
+        Some project
+    else
+        None
+
+let release =
+    let file = Path.Combine (projectTopDir, "RELEASE_NOTES.md")
+    LoadReleaseNotes file
+
+(*
+ * Configure output directories
+ *)
+
+let outputDir =
+        let desiredOutputDir = Environment.GetEnvironmentVariable("OUTPUT_DIRECTORY")
+        if System.String.IsNullOrWhiteSpace(desiredOutputDir) then
+                __SOURCE_DIRECTORY__
+        else
+                desiredOutputDir
+
+let buildDir = Path.Combine(outputDir, "build")
+let testDir = Path.Combine(outputDir, "test")
+let deployDir= Path.Combine(outputDir, "deploy")
+
+(*
+ * Helper code
+ *)
+
+// Helper active pattern for project types
+let (|Fsproj|Csproj|Vbproj|Shproj|) (projFileName:string) =
+    match projFileName with
+    | f when f.EndsWith("fsproj") -> Fsproj
+    | f when f.EndsWith("csproj") -> Csproj
+    | f when f.EndsWith("vbproj") -> Vbproj
+    | f when f.EndsWith("shproj") -> Shproj
+    | _                           -> failwith (sprintf "Project file %s not supported. Unknown project type." projFileName)
 
 let hasFxCopCmd =
-        let useFxCop = getBuildParamOrDefault "UseFxCop" "TRUE"
-        match useFxCop.ToUpperInvariant() with
-        | "TRUE"  -> true
-        | "FALSE" -> false
-        | _       -> false
+    let useFxCop = getBuildParamOrDefault "UseFxCop" "TRUE"
+    match useFxCop.ToUpperInvariant() with
+    | "TRUE"  -> true
+    | "FALSE" -> false
+    | _       -> false
 
+
+(*
+ * Build rules
+ *)
 
 Target "Clean" (fun _ ->
         CleanDir buildDir
